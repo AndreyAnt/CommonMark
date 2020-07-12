d90fce0e5433c57ffc7642847fbeb6158c45f3a0
Andrey Antropov <antropov@morizo.ru>
2020-07-12 21:35:50 +0300

    set poducts

--- Package.swift
+++ Package.swift
@@ -5,7 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "CommonMark",
-    dependencies: [],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "CommonMark",
+            targets: ["CommonMark"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
     targets: [
         .target(
             name: "CommonMark",
